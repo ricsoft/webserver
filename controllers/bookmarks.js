5de89d5273b93b5e972ddb14eac23d2d0c960178
Richard <42780925+ricsoft@users.noreply.github.com>
2023-11-16 13:39:37 -0800

    update bookmark create

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -1,11 +1,6 @@
 import { methods } from "../utils/constants.js";
-import { createLink, createFolder } from "../db/bookmarks.js";
+import { create } from "../db/bookmarks.js";
 
 export async function post(req) {
   if (req.method === methods.create) return await create(req);
-}
-
-async function create(req) {
-  if (req.link) return await createLink(req);
-  // else return await createFolder(req)
 }
--- db/bookmarks.js
+++ db/bookmarks.js
@@ -3,27 +3,45 @@ import { connect } from "./db.js";
 
 export async function setupBookmarks(mongo, collNames) {
   if (!collNames.includes("bookmarks")) {
-    const Bookmarks = mongo.model("bookmarks", schema(mongo));
-    const links = new Bookmarks({
+    const bookmarks =
+      mongo.models.bookmarks || mongo.model("bookmarks", schema(mongo));
+    const links = new bookmarks({
       name: "links",
       root: true,
       data: [],
     });
+
     await links.save();
   }
 }
 
-export async function createLink(req) {
-  const filter = { name: "links", root: true };
-  const mongo = await connect();
-  const Bookmarks = mongo.model("bookmarks", schema(mongo));
+export async function create(req) {
+  let filter = { name: "links", root: true };
+  const newData = {
+    name: req.name,
+    link: req.link,
+  };
 
-  const document = await Bookmarks.findOne(filter);
-  const res = await Bookmarks.updateOne(filter, {
-    data: [...document.data, { name: req.name, link: req.link }],
+  const mongo = await connect();
+  const bookmarks =
+    mongo.models.bookmarks || mongo.model("bookmarks", schema(mongo));
+
+  if (!req.link) {
+    const folder = new bookmarks({
+      name: req.name,
+      data: [],
+    });
+
+    const folderRes = await folder.save();
+    newData.link = folderRes._id;
+    newData.isFolder = true;
+  }
+
+  const document = await bookmarks.findOne(filter);
+  const res = await bookmarks.updateOne(filter, {
+    data: [...document.data, newData],
   });
+  mongo.connection.close();
 
   return res.modifiedCount === 1;
 }
-
-export async function createFolder(req) {}

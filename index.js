e166ca63d8b40b93f668a6f83983fdedc6287704
Richard <42780925+ricsoft@users.noreply.github.com>
2023-11-21 20:42:56 -0800

    Add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import router from "./routes/routes.js";
 import express from "express";
+import cors from "cors";
 import { setupDb } from "./db/db.js";
 import "dotenv/config.js";
 
@@ -8,4 +9,5 @@ await setupDb();
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(cors());
 app.listen(process.env.PORT);
